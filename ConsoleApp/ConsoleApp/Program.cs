bdd0186ca448f56ae7cccf0f583f14149cb273c5
Schädler Fabian (IT-PTR-CEN1-BDE22 - Extern) <fabian.schaedler@sbb.ch>
2025-11-03 10:30:36 +0100

    changed Project Structure

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more informationusing System;
 
-Console.WriteLine("Hello, World!");
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Hallo Welt");
+    }
+}
+ 
