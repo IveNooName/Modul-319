75874667a36c4cb7609cf1918bab37bc3e1a70cf
IveNooName <fsc159548@stud.gibb.ch>
2025-11-03 11:24:33 +0100

    changed Project structure

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -2,8 +2,13 @@
 
 class ConsoleApp {
     static void Main(string[] args) {
+        task1();
+    }
+
+    public static void task1() {
         Console.WriteLine("Modul 319 - Applikationen entwerfen und implementieren.");
         Console.WriteLine("Ich lerne die Programmiersprache C#.");
     }
+    
 }
  
