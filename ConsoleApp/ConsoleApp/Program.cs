8883c1c3176c9bdbc640cc85c9000e51ac93dea6
IveNooName <fsc159548@stud.gibb.ch>
2025-11-10 08:58:59 +0100

    Task 2.2.4b is finished - Working, except in the Rider Terminal

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,14 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more informationusing System;
 
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+
 class ConsoleApp {
     static void Main(string[] args) {
-        task1();
+        // task1();
+        task2_2_4_b();
+        
     }
 
     public static void task1() {
         Console.WriteLine("Modul 319 - Applikationen entwerfen und implementieren.");
         Console.WriteLine("Ich lerne die Programmiersprache C#.");
     }
-    
+
+    public static void task2_1_2_e() {
+        String fruit = "Apfel";
+        int value = 1000555;
+        bool isValid = true;
+        char letter = 'Z';
+
+    }
+    public static void task2_1_2_f() {
+        long x = 2147483648;
+        float pi = 3.14f;
+        decimal preis = 9.99m;
+        bool aktiv = true;
+
+    }
+
+    public static void task2_1_2_g() {
+        int number;
+        number = 7;
+        int result = number * 3;
+
+    }
+    public static void task2_2_4_b() {
+        Console.Write("Wie heisst du? ");
+        string name = Console.ReadLine();
+        Console.WriteLine("\n" + name);
+    }
 }
  
