bf467a7b52bd710f1981ddfcea91b6fd921f585e
IveNooName <fsc159548@stud.gibb.ch>
2025-11-17 12:22:07 +0100

    Made larger Changes: Rename an entire Projectfolder, Tasks2.1-2.3 -> MasterTask2.1 Added MasterTask2.3 Finished MasterTask2.3

---
+++ MasterTask2.3/MasterTask2.3/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class main {
+    static void Main(string[] args) {
+        
+        Console.Write("Gib den Radius des Kreises in cm ein: ");
+        String intputRadius = Console.ReadLine();
+        
+        if (Double.TryParse(intputRadius, out Double radius)) {
+            double resultScope = 2 * radius * Math.PI;
+            double resultArea = (radius * radius) * Math.PI;
+
+            Console.WriteLine($"Kreisradius: {radius} cm \nUmfang: {resultScope:F2} cm \nFläche: {resultArea:F2} cm²");
+        }
+        else {
+            Console.Error.WriteLine("\nUngültige Eingabe.");
+        }
+        
+    }
+}
