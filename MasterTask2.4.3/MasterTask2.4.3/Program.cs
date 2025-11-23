63e36e31977ea75eef8b943a41e11da1f7630382
IveNooName <fsc159548@stud.gibb.ch>
2025-11-23 18:51:35 +0100

    Finished MasterTask2.4.3

--- MasterTask2.4.3/MasterTask2.4.3/Program.cs
+++ MasterTask2.4.3/MasterTask2.4.3/Program.cs
@@ -1,25 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 class main {
-    static void Main(string[] args) {
-        Console.Write("Was ist deine Lieblingsfarbe? ");
-        String inputColor = Console.ReadLine();
+    public static void Main(string[] args) {
+        Console.Write("Bitte Schulnote eingeben (Deutsches Notensystem): ");
+        String inputGrade = Console.ReadLine();
+        Console.WriteLine(Environment.NewLine);
 
-        Console.WriteLine();
-
-        switch (inputColor) {
-            case "rot":
-                Console.WriteLine("Rot ist eine sehr kräftige Farbe!");
-                break;
-            case "blau":
-                Console.WriteLine("Blau wirkt beruhigend.");
-                break;
-            case "grün":
-                Console.WriteLine("Grün erinnert an die Natur.");
-                break;
-            default:
-                Console.WriteLine("Diese Farbe kenne ich nicht.");
-                break;
+        if (int.TryParse(inputGrade, out int grade)) {
+            switch (grade) {
+                case 1:
+                    Console.WriteLine("Sehr gut");
+                    break;
+                case 2:
+                    Console.WriteLine("Gut");
+                    break;
+                case 3:
+                    Console.WriteLine("Befriedigend");
+                    break;
+                case 4:
+                    Console.WriteLine("Ausreichend");
+                    break;
+                case 5:
+                    Console.WriteLine("Mangelhaft");
+                    break;
+                case 6:
+                    Console.WriteLine("Ungenügend");
+                    break;
+                
+                default:
+                    Console.WriteLine("Ungültige Note!");
+                    break;
+            }
+        } else {
+            Console.WriteLine("Das ist keine Zahl!");
         }
     }
 }
