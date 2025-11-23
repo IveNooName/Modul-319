e48865cae9964240b618e902e81ec562f5113523
IveNooName <fsc159548@stud.gibb.ch>
2025-11-23 20:12:47 +0100

    FInished MasterTask4.3.4

---
+++ MasterTask4.3.4/MasterTask4.3.4/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class MainClass {
+    public static void Main(string[] args) {
+
+        String password = "HowToDeleteSystem32";
+        
+        Console.Write("Bitte gib das Passwort ein: ");
+        String inputPassword = Console.ReadLine();
+        Console.WriteLine(Environment.NewLine);
+
+        while (!password.Equals(inputPassword)) {
+            Console.WriteLine("Falsches Passwort. Versuche es erneut.");
+            Console.Write("Bitte gib das Passwort ein: ");
+            inputPassword = Console.ReadLine();
+        }
+
+        Console.WriteLine("Zugriff erlaubt!");
+    }
+}
