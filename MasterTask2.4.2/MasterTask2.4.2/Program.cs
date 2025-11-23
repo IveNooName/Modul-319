6e2ace84db76bcf4b5261836ea8a83c57b2759d2
IveNooName <fsc159548@stud.gibb.ch>
2025-11-23 18:11:32 +0100

    Finished MasterTask2.4.2

---
+++ MasterTask2.4.2/MasterTask2.4.2/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 90% of the code comes from "daeswf/08_methods/src/main/java/ch/itninja/labs/basicexercises/NumberHelper.java" 
+
+class main {
+    static void Main(string[] args) {
+        int firstNumber = 5;
+        int secondNumber = 9;
+        int thirdNumber = 3;
+
+        int result1 = 0;
+        int result2 = 0;
+
+        if (firstNumber < secondNumber) {
+            result1 = firstNumber;
+        } else if (firstNumber > secondNumber) {
+            result1 = secondNumber;
+        }
+        
+        if (firstNumber < thirdNumber) {
+            result2 = firstNumber;
+        } else if (firstNumber > thirdNumber) {
+            result2 = thirdNumber;
+        }
+
+        
+        if (result1 < result2) {
+            // return result2;
+            
+        } else if (result1 > result2) {
+            // return result2;
+        }
+    }
+}
