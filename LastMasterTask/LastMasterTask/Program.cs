fc77a63ba7a207d9f0698186eeae005a0049e6a3
IveNooName <fsc159548@stud.gibb.ch>
2025-12-01 08:54:56 +0100

    Last MasterTask finished

---
+++ LastMasterTask/LastMasterTask/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * This Method calculate the 7er row.
+ * It goes max to 7*20
+ */
+
+class MainClass {
+    public static void Main(string[] args) {
+        for (int i = 1; i <= 20; i++) {
+            Console.WriteLine($"7 * {i} = {7 * i}");
+        }
+    }
+}
