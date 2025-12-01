50c6788c78bacef4b12d82845e76112ec87095a2
IveNooName <fsc159548@stud.gibb.ch>
2025-12-01 10:12:18 +0100

    Finished Task 4

--- TasksChapter5/TasksChapter5/Program.cs
+++ TasksChapter5/TasksChapter5/Program.cs
@@ -1,11 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-class  Program
-{
+class Program {
     static void Main(string[] args) {
         sayHello();
-        divideByTwo(67);
+        divideByTwo(134);
         addTwoDigitsTogether(66, 1);
+        isOfLegalAge(67);
+        sayHelloWithName("Ruby");
     }
 
     private static void sayHello() {
@@ -15,8 +16,16 @@ class  Program
     private static double divideByTwo(double input) {
         return input / 2;
     }
-    
+
     private static int addTwoDigitsTogether(int digit1, int digit2) {
-        return digit1 + digit2; 
+        return digit1 + digit2;
+    }
+
+    private static bool isOfLegalAge(int age) {
+        return age >= 18;
+    }
+
+    private static void sayHelloWithName(String name) {
+        Console.WriteLine("Hello " + name + "!");
     }
 }
