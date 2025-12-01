5b13a49301c0f6985985cef522f5e06c88b1ff90
IveNooName <fsc159548@stud.gibb.ch>
2025-12-01 10:45:53 +0100

    fix smartlearn code 3

---
+++ FixBadSmartlearnCode3/FixBadSmartlearnCode3/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program {
+    static void Main(string[] args) {
+        calculateSurface();
+    }
+
+    public static double getLength() {
+        Console.WriteLine("Gib die Länge des Rechtecks ein:");
+        return Convert.ToDouble(Console.ReadLine());
+    }
+
+    public static double getWidth() {
+        Console.WriteLine("Gib die Breite des Rechtecks ein:");
+        return Convert.ToDouble(Console.ReadLine());
+    }
+
+    public static void calculateSurface() {
+        double lenght = getLength();
+        double width = getWidth();
+
+        Console.WriteLine("Fläche: " + lenght * width);
+        Console.WriteLine("Umfang: " + 2 * (width + lenght));
+    }
+}
