d49f4ddbc78401f203e40debe5087483582cf1a5
IveNooName <fsc159548@stud.gibb.ch>
2025-12-01 08:41:52 +0100

    Finished MasterTask2.5.2

---
+++ MasterTask2.5.2/MasterTask2.5.2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("{0,5} | {1,8} | {2,10}", "Zahl", "Quadrat", "Wurzel");
+Console.WriteLine(new string('-', 30));
+
+for (int i = 0; i <= 100; i++) {
+    Console.WriteLine("{0,5} | {1,8} | {2,10}", i, i * i, Math.Round(Math.Sqrt(i), 8));
+    Console.WriteLine(new string('-', 30));
+}
