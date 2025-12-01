118409c9a823cba93790d57a0f142d4c67576bd7
IveNooName <fsc159548@stud.gibb.ch>
2025-12-01 09:53:16 +0100

    Task 1 and 2 finished

---
+++ TasksChapter5/TasksChapter5/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class  Program
+{
+    static void Main(string[] args) {
+        sayHello();
+        divideByTwo(67);
+    }
+
+    private static void sayHello() {
+        Console.WriteLine("Hallo und willkommen!");
+    }
+
+    private static double divideByTwo(double input) {
+        return input / 2;
+    }
+}
