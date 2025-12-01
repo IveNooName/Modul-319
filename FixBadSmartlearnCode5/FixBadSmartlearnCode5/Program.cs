72c865b9175c129f6321b1cd269839f1dd7dcbd6
IveNooName <fsc159548@stud.gibb.ch>
2025-12-01 10:55:39 +0100

    fixed bad smartlearn code 5

---
+++ FixBadSmartlearnCode5/FixBadSmartlearnCode5/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program {
+    static void Main(string[] args) {
+        int personCounter = 3;
+        double pricePerTicket = 11.50;
+
+        calculateTotalPrice(personCounter, pricePerTicket);
+    }
+
+
+    static void calculateTotalPrice(int personCounter,  double pricePerTicket) {
+        double totalPrice = personCounter * pricePerTicket;
+        Console.WriteLine("Gesamtpreis: " + totalPrice + " CHF");
+    }
+}
