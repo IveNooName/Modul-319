c03864962381b3b24422766a5fcbfefa77f747b3
IveNooName <fsc159548@stud.gibb.ch>
2025-12-01 08:48:55 +0100

    Finished MasterTask2.5.3

---
+++ MasterTask2.5.3/MasterTask2.5.3/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("GIb eine Zeichenfolge ein: ");
+String input = Console.ReadLine();
+
+Console.WriteLine("Zeichen und ihre Hexadezimalwerte: ");
+foreach (char c in input) {
+    Console.WriteLine("{0} -> 0x{1:X4}", c, (int)c);
+}
