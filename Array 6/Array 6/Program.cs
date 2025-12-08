ab7f2205ce8e603165702f39759bffba46c7f9ce
IveNooName <fsc159548@stud.gibb.ch>
2025-12-08 09:28:24 +0100

    Finished Array Task 6

---
+++ Array 6/Array 6/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class MainClass {
+    public static void Main() {
+        int max = 30;
+        bool[] prim = new bool[max + 1];
+
+
+        for (int i = 2; i < max; i++) {
+            prim[i] = true;
+        }
+
+        for (int i = 2; i < max; i++) {
+            if (prim[i]) {
+                for (int j = i; j < max; j++) {
+                    if (j == i) {
+                        continue;
+                    }
+
+                    if (j % i == 0) {
+                        prim[j] = false;
+                    }
+                }
+            }
+        }
+
+        //PRINT
+        for (int b = 0; b <= max; b++) {
+            Console.Write(b + ": " + (prim[b] ? "PRIM" : "—") + "\n");
+        }
+    }
+}
