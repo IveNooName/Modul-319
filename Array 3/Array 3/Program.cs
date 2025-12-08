24608c8a354ea4442fca4076e7ef8735d012075e
IveNooName <fsc159548@stud.gibb.ch>
2025-12-08 08:33:31 +0100

    finished Array Task 3

---
+++ Array 3/Array 3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class MainClass {
+    public static void Main(string[] args) {
+        string[] animals = { "Luchs", "Wolf", "Maus", "Fuchs", "Eichhörnchen", "Waldkauz", "Eule", "Reh", "Wildsau" };
+
+        foreach (var t in animals) {
+            if (t.Length > 4) {
+                Console.Write(t + " ");
+            }
+        }
+    }
+}
