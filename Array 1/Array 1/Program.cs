06197c499bf4ad9cee6238c5d45904ff733039fc
IveNooName <fsc159548@stud.gibb.ch>
2025-12-08 08:20:13 +0100

    Finished Array Task 1

---
+++ Array 1/Array 1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class MainClass
+{
+	public static void Main(string[] args) {
+		
+		int[] numberArray = {7, 11, 6, 55, 98, 45, 16, 96, 46}; 
+		
+		Array.Sort(numberArray);
+	}
+}
+
