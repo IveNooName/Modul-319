2f6de069e9b042196c0ad7a9a5bc00a4e11058e0
IveNooName <fsc159548@stud.gibb.ch>
2025-12-08 09:30:44 +0100

    finished Array 6

--- Array 6/Array 6/Program.cs
+++ Array 6/Array 6/Program.cs
@@ -2,31 +2,35 @@
 
 public class MainClass {
     public static void Main() {
-        int max = 30;
-        bool[] prim = new bool[max + 1];
+        Console.Write("Zahl n: ");
+
+        if (int.TryParse(Console.ReadLine(), out int max)) {
+
+            bool[] prim = new bool[max + 1];
 
 
-        for (int i = 2; i < max; i++) {
-            prim[i] = true;
-        }
+            for (int i = 2; i < max; i++) {
+                prim[i] = true;
+            }
 
-        for (int i = 2; i < max; i++) {
-            if (prim[i]) {
-                for (int j = i; j < max; j++) {
-                    if (j == i) {
-                        continue;
-                    }
+            for (int i = 2; i < max; i++) {
+                if (prim[i]) {
+                    for (int j = i; j < max; j++) {
+                        if (j == i) {
+                            continue;
+                        }
 
-                    if (j % i == 0) {
-                        prim[j] = false;
+                        if (j % i == 0) {
+                            prim[j] = false;
+                        }
                     }
                 }
             }
-        }
 
-        //PRINT
-        for (int b = 0; b <= max; b++) {
-            Console.Write(b + ": " + (prim[b] ? "PRIM" : "—") + "\n");
+            //PRINT
+            for (int b = 0; b <= max; b++) {
+                Console.Write(b + ": " + (prim[b] ? "PRIM" : "—") + "\n");
+            }
         }
     }
 }
