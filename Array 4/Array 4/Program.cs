c77bddb74b9f945c61b649860a520a07e4465dfb
IveNooName <fsc159548@stud.gibb.ch>
2025-12-08 08:43:01 +0100

    Finished Array Task 4

---
+++ Array 4/Array 4/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Array_4 {
+    public static void Main() {
+
+        Console.Write("Eingabe: ");
+        String word = Console.ReadLine();
+        char[] chars = word.ToCharArray();
+        char[] changedOrder = new char[chars.Length];
+
+        for (int i = 0; i < chars.Length; i++) {
+            changedOrder[changedOrder.Length - 1 - i] = chars[i];
+        }
+
+        foreach (char item in changedOrder) {
+            Console.Write(item);
+        }
+    }
+}
